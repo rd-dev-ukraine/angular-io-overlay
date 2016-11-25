f80751b1f2c8495dcc20468867e320a20de6cdf6
Igor Turko <igor.turko@rd-dev.net>
2016-11-25 11:56:57 +0200

    updated

--- src/app/datepicker/index.ts
+++
@@ -1,6 +0,0 @@
-export { DatePickerMode } from "./common";
-export * from "./datePicker";
-export * from "./datePickerPanel";
-export * from "./dateSelector";
-export * from "./timeSelector";
-export * from "./datepicker.module";
